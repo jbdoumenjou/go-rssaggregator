4c314ad20b72a4ff4472df01916f16178f2ad746
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2024-01-28 22:16:30 +0100

    enhance database, reapply sqlc, add apikey

--- handler_user.go
+++ handler_user.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
-	"time"
+	"strings"
 
-	"github.com/google/uuid"
 	"github.com/jbdoumenjou/go-rssaggregator/internal/database"
 )
 
@@ -34,13 +36,7 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	now := time.Now().UTC().Round(time.Microsecond)
-	user, err := h.db.CreateUser(r.Context(), database.CreateUserParams{
-		ID:        uuid.New(),
-		Name:      req.Name,
-		CreatedAt: now,
-		UpdatedAt: now,
-	})
+	user, err := h.db.CreateUser(r.Context(), req.Name)
 	if err != nil {
 		// Error should be filtered here.
 		slog.Log(r.Context(), slog.LevelError, "create user: %v", err)
@@ -48,4 +44,42 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, user)
+}
+
+// GetUser get a user from the authorization header.
+func (h *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
+	token, err := getAuthHeader(r.Header)
+	if err != nil || token == "" {
+		slog.Log(r.Context(), slog.LevelInfo, "get auth header: %s", err)
+		respondWithError(w, http.StatusForbidden, http.StatusText(http.StatusForbidden))
+		return
+	}
+
+	user, err := h.db.GetUserFromApiKey(r.Context(), token)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			respondWithError(w, http.StatusForbidden, http.StatusText(http.StatusForbidden))
+			return
+		}
+		// Error should be filtered here.
+		slog.Log(r.Context(), slog.LevelError, "get user: %v", err)
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, user)
+}
+
+func getAuthHeader(h http.Header) (string, error) {
+	authHeader := h.Get("Authorization")
+	if authHeader == "" {
+		return "", fmt.Errorf("missing authorization header")
+	}
+
+	split := strings.Split(authHeader, " ")
+	if strings.ToLower(split[0]) != "apikey" {
+		return "", fmt.Errorf("invalid authorization header")
+	}
+
+	return split[1], nil
 }
